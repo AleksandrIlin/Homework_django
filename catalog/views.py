55689bf09b6708fd62b83d3c9a11454ebdf450ee
Aleksandr <aqwa06@gmail.com>
2024-09-20 04:27:26 +0500

    Writing code in views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,13 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
-
-def home(request):
-    return render(request, 'home.html')
+from catalog.models import Category, Product
 
 
-# def contacts(request):
-#     return render(request, 'contacts.html')
+# def home(request):
+#     return render(request, 'home.html')
 
 
 def contacts(request):
@@ -17,3 +15,22 @@ def contacts(request):
 
         return HttpResponse(f"Спасибо, {name}. Сообщение получено.")
     return render(request, 'contacts.html')
+
+
+def category_list(request):
+    categories = Category.objects.all()
+    context = {"categories": categories}
+    return render(request, 'category_list.html', context)
+
+
+def product_list(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, 'product_list.html', context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, 'product_detail.html', context)
+
