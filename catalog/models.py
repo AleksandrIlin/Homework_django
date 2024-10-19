4a372770649585345cd47ca5eb156f888c92aeef
Aleksandr <aqwa06@gmail.com>
2024-10-19 21:08:08 +0500

    Changing model product

--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from users.models import CustomUser
 
 
 class Category(models.Model):
@@ -22,6 +23,8 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0.00)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    publication_status = models.BooleanField(default=False)
+    owner = models.ForeignKey(CustomUser, verbose_name='Владелец', blank=True, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f"{self.name} - {self.description}"
@@ -30,3 +33,6 @@ class Product(models.Model):
         verbose_name = 'продукт'
         verbose_name_plural = 'продукты'
         ordering = ['name']
+        permissions = [
+            ("can_unpublish_product", "can unpublish product"),
+        ]
